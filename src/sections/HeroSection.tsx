f1d27d7d64888da7808ca19cfce090f79d1aeb24
Jules Maestro <jlsodai@gmail.com>
2023-12-22 13:27:16 +0000

    fix home page responsiveness

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -80,27 +80,31 @@ const HeroSection = ({ links }: Props) => {
 
         <div className="flex flex-1 items-center bg-cover bg-[url('https://res.cloudinary.com/dmaestro/image/upload/v1703185993/kitchen_sttepa.jpg')]">
           <div className="flex flex-1 py-12 text-gray-800 container items-center">
-            <div className="bg-white/80 p-16 space-y-4 h-full">
-              <h2 className="font-bold text-7xl">Elevate Spaces,</h2>
-              <h3 className="text-5xl">Craft Dreams</h3>
+            <div className="bg-white/80 p-8 lg:p-16 space-y-2 lg:space-y-4 h-full">
+              <h2 className="font-bold text-3xl lg:text-7xl">
+                Elevate Spaces,
+              </h2>
+              <h3 className="text-2xl lg:text-5xl">Craft Dreams</h3>
               <div className="bg-gray-800 w-[120px] h-[10px]">&nbsp;</div>
             </div>
           </div>
         </div>
 
-        <div className="py-10 herohalf container flex items-center">
-          <h1 className="flex-1">
-            <a
-              href="#"
-              className="px-8 hover:bg-primary text-white bg-gray-800 py-3"
-            >
-              Contact Us
-            </a>
-          </h1>
-          <div className="w-[55%] text-right">
-            We are manufacturers of wood solutions for all kinds of spaces:
-            <br />
-            cabinetry, furniture and all carpentry designs.
+        <div className="lg:bg-transparent  bg-[#ededed]">
+          <div className="py-10 herohalf container lg:flex items-center">
+            <div className="lg:w-[45%] md:text-right order-2">
+              We are manufacturers of wood solutions for all kinds of spaces:
+              cabinetry, furniture and all carpentry designs.
+            </div>
+
+            <h1 className="flex-1 order-1 -sm:text-center -sm:mt-8">
+              <a
+                href="#"
+                className="px-8 hover:bg-primary text-white bg-gray-800 py-3"
+              >
+                Contact Us
+              </a>
+            </h1>
           </div>
         </div>
       </section>
