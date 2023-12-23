28f452eb723c0bf4f4fa25ae6cd5e4f0639e68e7
Jules Maestro <jlsodai@gmail.com>
2023-12-23 21:28:40 +0000

    add contact link on home page

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -66,88 +66,16 @@ const HeroSection = ({ links }: Props) => {
             </div>
 
             <h1 className="flex-1 order-1 -sm:text-center -sm:mt-8">
-              <a href="#">
+              <Link href="/contact-us">
                 <span className="px-8 hover:bg-primary text-white bg-gray-800 py-3">
                   Contact Us
                 </span>
                 <span className="bg-[#ededed] p-3">→</span>
-              </a>
+              </Link>
             </h1>
           </div>
         </div>
       </section>
-
-      {/* <section
-        className="flex flex-col justify-between min-h-screen bg-[url('https://res.cloudinary.com/tbra/image/upload/v1683041089/EgyptHeroBg_uv3bcq.jpg')] bg-cover bg-center text-white"
-        id="top"
-      >
-        <div className="container mt-8 flex justify-between items-center">
-          <Link href="/" className="">
-            <img
-              src="https://theboardroomafrica.com/images/logo-primary.svg"
-              className="img-white"
-              width="100px"
-              alt="Logo"
-            />
-          </Link>
-          <div className="-sm:hidden">
-            <div className="flex divide-x -sm:mt-8 -sm:justify-center">
-              {links.map((link, i) => (
-                <Link
-                  href={link.url}
-                  className="px-6 hover:underline"
-                  key={i}
-                  onClick={handleScroll}
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="container -md:my-16 md:grid grid-cols-2 items-center">
-          <div className="">
-            <h3 className="text-mustard font-libreb text-2xl">
-              TBrA Annual Gathering
-            </h3>
-            <h1 className="text-8xl -sm:text-6xl mt-2 mb-8 font-halyard font-thin">
-              Egypt 2023
-            </h1>
-            <p className="max-w-sm my-4">
-              Join us for a week of networking opportunities, curated workshops
-              and rich cultural & culinary experiences.
-            </p>
-          </div>
-          <iframe
-            className="-md:hidden"
-            width="100%"
-            height="400"
-            src="https://www.youtube.com/embed/K_JUuffs0g4?si=Tt64JroPZliI-4KQ"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className="glassmorphism">
-          <div className="container md:flex justify-between items-center my-4">
-            <p className="border-l-8 py-2 px-8 border-mustard font-libreb">
-              22 - 28 October 2023
-              <br />
-              Cairo & Sharm El-Sheikh, Egypt
-            </p>
-          </div>
-        </div>
-        <Link
-          href="#top"
-          title="Go To Top"
-          onClick={handleScroll}
-          className={`${
-            !showTop && "hidden"
-          } fixed z-90 bottom-8 right-8 border-0 w-16 h-16 rounded-full drop-shadow-md bg-tender text-white text-2xl font-bold flex justify-center items-center`}
-        >
-          &uarr;
-        </Link>
-      </section> */}
     </>
   );
 };
