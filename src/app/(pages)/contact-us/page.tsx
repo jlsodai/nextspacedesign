009c8d3e621f193798349076dbc71193e457c006
Jules Maestro <jlsodai@gmail.com>
2023-12-23 17:35:24 +0000

    add form to contact page - test netlify

--- src/app/(pages)/contact-us/page.tsx
+++ src/app/(pages)/contact-us/page.tsx
@@ -1,10 +1,12 @@
 import PageTitle from "@/components/PageTitle";
+import ContactSection from "@/sections/ContactSection";
 import React from "react";
 
 const ContactPage = () => {
   return (
     <>
       <PageTitle title="Contact Us" />
+      <ContactSection />
     </>
   );
 };
---
+++ src/sections/ContactSection.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+const ContactSection = () => {
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-sm">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+          Leave a message or call us
+        </h2>
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          +233 20 502 1111
+          <br />
+          antoine@nextspacedesign.com
+          <br />
+          PO Box 2224, Sakumono, Accra, Ghana
+        </p>
+        <form action="#" className="space-y-8" netlify>
+          <div className="grid md:grid-cols-2 gap-8">
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email *
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary dark:shadow-sm-light"
+                placeholder="name@flowbite.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Full name *
+              </label>
+              <input
+                type="text"
+                id="subject"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary dark:shadow-sm-light"
+                placeholder="Your full name"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Phone Number *
+              </label>
+              <input
+                type="text"
+                id="phone"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary dark:shadow-sm-light"
+                placeholder="Phone Number"
+                required
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="message"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              Your message
+            </label>
+            <textarea
+              id="message"
+              rows={6}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary focus:border-primary dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
+              placeholder="Leave a comment..."
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary sm:w-fit hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary"
+          >
+            Send message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ContactSection;
