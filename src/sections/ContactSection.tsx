ef3b8f29c8fafb9e839b7bd7fe87bc2bb97b9da0
Jules Maestro <jlsodai@gmail.com>
2023-12-23 21:45:19 +0000

    update email placeholder

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -39,7 +39,7 @@ const ContactSection = () => {
                 id="email"
                 name="email"
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5"
-                placeholder="name@flowbite.com"
+                placeholder="Email address"
                 required
               />
             </div>
