8b211f505cdac99b60bfda39ee9e5c8af84167dd
Jules Maestro <jlsodai@gmail.com>
2023-12-29 07:47:20 +0000

    crop header section bg image

--- src/app/(pages)/gallery/page.tsx
+++ src/app/(pages)/gallery/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const GalleryPage = () => {
   return (
     <>
-      <section className="bg-cover bg-[url('https://res.cloudinary.com/dmaestro/image/upload/v1703770678/color1_wiiswn.jpg')]">
+      <section className="bg-cover bg-[url('https://res.cloudinary.com/dmaestro/image/upload/w_800,h_300,g_north,c_fill/v1703770678/color1_wiiswn.jpg')]">
         <div className="container h-56 text-white flex items-center">
           <h2 className="text-5xl">Our Gallery</h2>
         </div>
--- src/app/(pages)/services/page.tsx
+++ src/app/(pages)/services/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const ServicesPage = () => {
   return (
     <>
-      <section className="bg-cover bg-[url('https://res.cloudinary.com/dmaestro/image/upload/v1703770679/color2_l6uxxx.jpg')]">
+      <section className="bg-cover bg-top bg-[url('https://res.cloudinary.com/dmaestro/image/upload/w_800,h_300,g_north,c_fill/v1703770679/color2_l6uxxx.jpg')]">
         <div className="container h-56 text-white flex items-center">
           <h2 className="text-5xl">Our Services</h2>
         </div>
